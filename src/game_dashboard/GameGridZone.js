544233bf32ecd583a608b03edcdde65d3f101e10
Piotr <piotr.napierala@intracto.com>
2021-02-21 12:04:13 +0100

    Finally, the blocks logic is finished ...

--- src/game_dashboard/GameGridZone.js
+++ src/game_dashboard/GameGridZone.js
@@ -1,15 +1,41 @@
-// import { useState, useCallback } from "react";
+import { useState } from "react";
 import ImageGridElement from "./ImageGridElement";
 
 export default function GameGridZone({ images, gridSize }) {
+  const [flipped, setFlipped] = useState([]);
+  const [currentImageId, setCurrentImageId] = useState(null);
+  const [resolved, setResolved] = useState([]);
+
+  const onClickHandler = e => {
+    if (flipped.length === 0) {
+      if (resolved.includes(e.target.id)) return;
+      setFlipped([e.target.dataset.gridIndex]);
+      setCurrentImageId(e.target.id);
+    } else if (
+      flipped.length > 0 &&
+      !flipped.includes(e.target.dataset.gridIndex)
+    ) {
+      if (e.target.id === currentImageId) {
+        setResolved([...resolved, e.target.id]);
+      }
+      setFlipped([...flipped, e.target.dataset.gridIndex]);
+      setCurrentImageId(null);
+      setTimeout(() => setFlipped([]), 1000);
+    }
+  };
+
   return (
     <div className={`grid grid-cols-${gridSize} gap-3`}>
       {images.map((image, i) => {
         return (
           <ImageGridElement
+            resolved={resolved.includes(image.id)}
+            flipped={flipped.includes(String(i))}
+            gridIndex={i}
             gridSize={gridSize}
             key={i.toString()}
             image={image}
+            onClickHandler={onClickHandler}
           />
         );
       })}
--- src/game_dashboard/ImageGridElement.js
+++ src/game_dashboard/ImageGridElement.js
@@ -1,34 +1,38 @@
-import { useState } from "react";
-
-export default function ImageGridElement({ image, gridSize }) {
-  const [flipped, setFlipped] = useState(false);
-
-  const hoverEvent = () => {
-    setFlipped(!flipped);
-  };
-
-  var cardSize = 760 / gridSize + "px";
+export default function ImageGridElement({
+  onClickHandler,
+  gridIndex,
+  image,
+  gridSize,
+  flipped,
+  resolved
+}) {
+  var cardSize = 760 / gridSize;
+  console.log(flipped);
 
   return (
-    <div className="perspective" onClick={() => hoverEvent()}>
+    <div className="perspective" onClick={e => onClickHandler(e)}>
       <div
         className={`preserve-3d duration-500 relative ${
-          flipped ? "rotate-y-180" : ""
+          flipped || resolved ? "rotate-y-180" : ""
         }`}
       >
         <div
-          className="bg-center bg-no-repeat bg-cover"
+          className="z-2 bg-center bg-no-repeat bg-cover backface-hidden rotate-y-180"
           style={{
             backgroundImage: `url(${image.download_url})`,
             width: cardSize,
             height: cardSize
           }}
           alt={image.author}
+          data-grid-index={gridIndex}
+          id={image.id}
         />
 
         <div
-          className="z-2 bg-purple-600 backface-hidden absolute top-0 left-0 transform rotate-y-180"
-          style={{ maxWidth: `${cardSize}`, maxHeight: `${cardSize}` }}
+          className="bg-purple-600 backface-hidden absolute top-0 left-0 transform"
+          style={{ width: cardSize, height: cardSize }}
+          data-grid-index={gridIndex}
+          id={image.id}
         ></div>
       </div>
     </div>
